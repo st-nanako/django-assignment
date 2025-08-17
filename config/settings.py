fd906e131a38f0f5e8bef29b2a7b7bbda6574454
stnanako <n.sato.sys24@morijyobi.ac.jp>
2025-08-17 18:30:22 +0900

    login correction

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ログイン後のリダイレクト先
+LOGIN_REDIRECT_URL = '/'
