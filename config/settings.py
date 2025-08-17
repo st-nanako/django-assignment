43416eb6f071aadc2ce02487a7a66f8bd0229458
stnanako <n.sato.sys24@morijyobi.ac.jp>
2025-08-17 17:38:54 +0900

    settings

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-LOGIN_REDIRECT_URL = '/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
